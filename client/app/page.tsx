8a1799e1d5b5d27041ac9d2925c97e64194009ac
DEV_BUDDY <allishittuabdulhameed@gmail.com>
2025-11-09 10:14:15 +0100

    setup complete

---
+++ client/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-sans">
+      <p> ORR site setup complete</p>
+    </div>
+  );
+}
