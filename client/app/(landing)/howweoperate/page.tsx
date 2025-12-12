49e0535629d5a27b6e34a8bb2bae9bcd4fb03cf9
Abdulluh <abdulluhodulate@gmail.com>
2025-12-12 04:14:28 +0100

    image

--- client/app/(landing)/howweoperate/page.tsx
+++ client/app/(landing)/howweoperate/page.tsx
@@ -259,7 +259,7 @@ export default function StickyScrollSplit() {
 
           {/* Right Side - Fixed Image */}
           <div className="block lg:relative">
-            <div className="fixed top-4 left-1/2 transform -translate-x-1/2 lg:top-32 lg:left-1/2 lg:transform lg:-translate-x-1/2 w-[65%] sm:w-[60%] lg:w-[75%] max-w-[350px] sm:max-w-[450px] lg:max-w-[900px] h-[35vh] sm:h-[45vh] lg:h-[calc(85vh-3rem)]">
+            <div className="fixed top-4 left-1/2 transform -translate-x-1/2 lg:top-32 lg:left-1/2 lg:transform lg:-translate-x-1/2 w-[85%] sm:w-[80%] lg:w-[75%] max-w-[500px] sm:max-w-[600px] lg:max-w-[900px] h-[35vh] sm:h-[45vh] lg:h-[calc(85vh-3rem)]">
               <div className="w-full h-full flex items-center">
                 <div className="relative w-full h-full rounded-xl lg:rounded-3xl overflow-hidden shadow-2xl">
                   <img
