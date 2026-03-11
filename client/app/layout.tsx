d1c486030329ce1821299f819c9e4de5bf6ad25f
OLOWONISHAYE SUNKANMI <skprojectx12@gmail.com>
2026-03-10 22:46:48 -0700

    new changes on hero section

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -127,9 +127,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-scroll-behavior="smooth">
+    <html lang="en" data-scroll-behavior="smooth" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <ThemeProvider>
           {children}
